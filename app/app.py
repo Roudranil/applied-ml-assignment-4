b6d8d4db5eb5c7ab0360d8202cca0be01b305499
Roudranil <nil0auditore78@gmail.com>
2024-03-12 10:35:51 +0530

    [code] added basic running code for app and the /score endpoint

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.score import Score
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Score, "/score")
+
+if __name__ == "__main__":
+    # Run the Flask app
+    app.run(debug=True, port=5000)
---
+++ app/resources/score.py
@@ -0,0 +1,14 @@
+from flask import json, request, jsonify
+from flask_restful import Resource
+from src.score import MODEL_PATH, score, load_model_vectoriser
+
+
+class Score(Resource):
+    def post(self):
+        model, vectoriser = load_model_vectoriser(MODEL_PATH)
+        text = request.json.get("text")
+        if text:
+            prediction, propensity = score(text, model, vectoriser, threshold=0.5)
+            return jsonify({"prediction": prediction, "propensity": propensity})
+        else:
+            return jsonify({"error": "No text provided"}), 400
